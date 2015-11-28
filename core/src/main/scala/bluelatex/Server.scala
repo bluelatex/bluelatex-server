4f447955c0d261dbcaf9f185b7d8061d5873796d
Lucas Satabin <lucas.satabin@gnieh.org>
2015-11-28 15:37:26 +0100

    Add basic server

---
+++ core/src/main/scala/bluelatex/Server.scala
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the \BlueLaTeX project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package bluelatex
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import com.typesafe.config.Config
+
+import scala.concurrent.Future
+
+import config._
+
+class Server(conf: Config) extends StdReaders {
+
+  implicit val system = ActorSystem("bluelatex")
+  implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
+
+  val route =
+    path("hello") {
+      get {
+        complete {
+          "toto"
+        }
+      }
+    }
+
+  private var bindingFuture: Future[Http.ServerBinding] = null
+
+  def start(): Unit =
+    if (bindingFuture == null)
+      bindingFuture = Http().bindAndHandle(route, conf.as[String]("bluelatex.http.host"), conf.as[Int]("bluelatex.http.port"))
+
+  def stop(): Unit =
+    if (bindingFuture != null) {
+      bindingFuture
+        .flatMap(_.unbind()) // trigger unbinding from the port
+        .onComplete(_ => system.shutdown()) // and shutdown when done
+      bindingFuture = null
+    }
+
+}
